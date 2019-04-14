4af6c7136dabbd3017488f22120ddd56dc3950ab
SilicaGel_SrD <makaron3141@gmail.com>
2019-04-14 13:07:49 +0900

    コミット

--- PinBall/Assets/Script/BallController.cs
+++ PinBall/Assets/Script/BallController.cs
@@ -8,16 +8,60 @@ public class BallController : MonoBehaviour {
 	private float visiblePosZ = -6.5f;
 
 	private GameObject gameoverText;
+	private GameObject scoreText;
 
-	// Use this for initialization
+	private int score_smlstr = 10;					/* 小さい星 */
+	private int score_lgestr = 20;					/* 大きい星 */
+	private int score_smlcld = 5;					/* 小さい雲 */
+	private int score_lgecld = 15;					/* 大きい雲 */
+	private int totalscore = 0;						/* 合計スコア */
+
 	void Start () {
 		this.gameoverText = GameObject.Find( "GameOverText" );
+		this.scoreText = GameObject.Find( "ScoreText" );
 	}
 	
-	// Update is called once per frame
 	void Update () {
 		if( this.transform.position.z < this.visiblePosZ ) {
 			this.gameoverText.GetComponent<Text>().text = "Game Over";
+		}
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		bool f_disp_ref;							/* テキスト更新有FLG */
+		string dtc_tag = collision.collider.tag;	/* 衝突対象のTagを取得 */
+		string disp_score = "Score：";				/* 画面表示文字列 */
+
+		//Debug.Log( dtc_tag );						/* TEST */
+
+		f_disp_ref = true;							/* デフォルトはテキスト更新有に設定 */
+		switch( dtc_tag ) {							/* 衝突対象に応じてスコアを加算 */
+			case "SmallStarTag":
+				totalscore += score_smlstr;
+				break;
+			case "LargeStarTag":
+				totalscore += score_lgestr;
+				break;
+			case "SmallCloudTag":
+				totalscore += score_smlcld;
+				break;
+			case "LargeCloudTag":
+				totalscore += score_lgecld;
+				break;
+			default:								/* 壁・フリッパの時はdefault */
+				f_disp_ref = false;					/* テキスト更新不要 */
+				break;
+		}
+		if( f_disp_ref ) {							/* テキスト更新が必要？ */
+			if( totalscore > 1000 ) {				/* 仕様なしだがOVF防止 */
+				totalscore = 1001;
+				disp_score += "1000over";
+			} else {
+				disp_score += totalscore;
+			}
+
+			this.scoreText.GetComponent<Text>().text = disp_score;
+													/* 画面に表示 */
 		}
 	}
 }
--- PinBall/Assets/Script/FripperController.cs
+++ PinBall/Assets/Script/FripperController.cs
@@ -6,38 +6,91 @@ public class FripperController : MonoBehaviour {
 
 	private HingeJoint myHingeJoint;
 
-	private float defaultAngle = 20;
-	private float flickAngle = -20;
+	private float flDefaultAngle = 20;
+	private float flFlickAngle = -20;
+
+	private bool boPush = false;
+
+	// ウィンドウ解像度の半分を取得
+	private float flScreenWidth = Screen.width / 2;
 
 	// Use this for initialization
 	void Start () {
 		this.myHingeJoint = GetComponent<HingeJoint>();
 
-		SetAngle( this.defaultAngle );
+		SetAngle( this.flDefaultAngle );
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if( Input.GetKeyDown(KeyCode.LeftArrow) && tag == "LeftFripperTag" ) {
-			SetAngle( this.flickAngle );
+
+		//左矢印キーを押した時左フリッパーを動かす
+		if(Input.GetKeyDown( KeyCode.LeftArrow ) && tag == "LeftFripperTag") {
+			SetAngle( this.flFlickAngle );
 		}
-		if (Input.GetKeyDown( KeyCode.RightArrow ) && tag == "RightFripperTag") {
-			SetAngle( this.flickAngle );
+		//右矢印キーを押した時右フリッパーを動かす
+		if(Input.GetKeyDown( KeyCode.RightArrow ) && tag == "RightFripperTag") {
+			SetAngle( this.flFlickAngle );
 		}
 
-		if (Input.GetKeyUp( KeyCode.LeftArrow ) && tag == "LeftFripperTag") {
-			SetAngle( this.defaultAngle );
+		//矢印キー離された時フリッパーを元に戻す
+		if(Input.GetKeyUp( KeyCode.LeftArrow ) && tag == "LeftFripperTag") {
+			SetAngle( this.flDefaultAngle );
 		}
-		if (Input.GetKeyUp( KeyCode.RightArrow ) && tag == "RightFripperTag") {
-			SetAngle( this.defaultAngle );
+		if(Input.GetKeyUp( KeyCode.RightArrow ) && tag == "RightFripperTag") {
+			SetAngle( this.flDefaultAngle );
 		}
 
 	}
+/*
+	int TouchJug() {
+		//だめだああああああああああああああああああああ
+		// Touchクラスの情報を取得(配列)
+		Touch[] TouchInfo = Input.touches;
 
+		int i;
+		bool fTapRight, fTapLeft;
+
+		fTapRight = false;
+		fTapLeft = false;
+
+		for( i = 0; i < Input.touchCount; i++ ) {
+			if( TouchInfo[i].position.x > flScreenWidth ) {
+				fTapRight = true;
+			} else if( TouchInfo[i].position.x >= 0) {
+				fTapLeft = true;
+			}
+		}
+
+		int inInputJug = 0;
+
+		if( fTapLeft ) {
+			inInputJug += 0x1;
+		}
+
+		if( fTapRight ) {
+			inInputJug += 0x2;
+		}
+
+		return inInputJug;
+	}
+
+	 KeyJug() {
+
+
+	}
+	*/
 	public void SetAngle( float angle ) {
-		JointSpring jointSpr = this.myHingeJoint.spring;	/* 現在値をローカルに取得 */
-		jointSpr.targetPosition = angle;					/* 入力するアングルをセット */
-		this.myHingeJoint.spring = jointSpr;				/* セットされた値をモデルに反映 */
+
+		// 現在値をローカルに取得
+		JointSpring jointSpr = this.myHingeJoint.spring;
+
+		// 入力するアングルをセット
+		jointSpr.targetPosition = angle;
+
+		// セットされた値をモデルに反映
+		this.myHingeJoint.spring = jointSpr;
 
 	}
 }
